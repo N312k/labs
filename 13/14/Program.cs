15c920da0b19e1df87047915f47d10808e9064da
N312k <ntashirov18@gmail.com>
2025-01-24 15:37:19 +0300

    Add files via upload

---
+++ 13/14/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using MyArrayDeque;
+namespace Lab15
+{
+    class program
+    {
+        public static int Kdigit(string line)
+        {
+            int count = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Char.IsDigit(line[i])) count++;
+            }
+            return count;
+        }
+        public static int Kspace(string line)
+        {
+            int count = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == ' ') count++;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string path1 = "D:/input.txt";
+            string path2 = "D:/sorted.txt";
+            StreamReader sr = new StreamReader(path1);
+            MyArrayDeque<string> deque = new MyArrayDeque<string>();
+            string line = sr.ReadLine();
+            if (line != null) { deque.Add(line); }
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                if (line != null)
+                {
+                    if (Kdigit(line) > Kdigit(deque.GetFirst())) deque.AddLast(line);
+                    else deque.AddFirst(line);
+                }
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter(path2);
+            for (int i = deque.IndexOfHead(); i < deque.Size(); i++)
+            {
+                sw.WriteLine(deque.Get(i));
+            }
+            sw.Close();
+
+            Console.WriteLine("Введите кол-во пробелов: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = deque.IndexOfHead(); i < deque.Size(); i++)
+            {
+                if (Kspace(deque.Get(i)) > N)
+                {
+                    deque.Remove(deque.Get(i));
+                    i--;
+                }
+            }
+            for (int i = deque.IndexOfHead(); i < deque.Size(); i++)
+                Console.WriteLine(deque.Get(i));
+        }
+    }
+}
